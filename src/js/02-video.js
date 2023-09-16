4f62e0d2d27a36681ab02dc8a3e95c8bae9fe337
RomanMytsu <132835114+RomanMytsu@users.noreply.github.com>
2023-09-16 17:09:05 +0300

    v1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,15 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+const LS_TIME_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+  const currentTime = data.seconds;
+  localStorage.setItem(LS_TIME_KEY, currentTime);
+}
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(LS_TIME_KEY)) || 0);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+import throttle from 'lodash.throttle';
 
+const LS_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', throttle(onInputData, 500));
+form.addEventListener('submit', onSubmit);
+
+let dataForm = JSON.parse(localStorage.getItem(LS_KEY)) || {};
+const { email, message } = form.elements;
+reloadPage();
+
+function onInputData() {
+  dataForm = { email: email.value, message: message.value };
+  localStorage.setItem(LS_KEY, JSON.stringify(dataForm));
+}
+
+function reloadPage() {
+  if (dataForm) {
+    email.value = dataForm.email || '';
+    message.value = dataForm.message || '';
+  }
+}
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  console.log(dataForm);
+  evt.currentTarget.reset();
+  localStorage.removeItem(LS_KEY);
+}
